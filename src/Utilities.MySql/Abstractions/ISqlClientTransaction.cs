10b8404eb9acb7f68834b9c7a4907c4fd13bd2b6
Jacob Schwartz <schwja04@gmail.com>
2022-10-04 23:53:05 -0500

    WIP
    sqlhelper executereaderasync is not using the async command calll currently

---
+++ src/Utilities.MySql/Abstractions/ISqlClientTransaction.cs
@@ -0,0 +1,10 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Utilities.MySql.Abstractions
+{
+    internal interface ISqlClientTransaction
+    {
+        MySqlTransaction SqlClientTransaction { get; }
+    }
+}
+
