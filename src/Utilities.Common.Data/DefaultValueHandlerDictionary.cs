8db59ef4c59754c6092530ffbd5cb468c7c3c8dc
Jacob Schwartz <schwja04@gmail.com>
2022-10-05 21:00:42 -0500

    Pulled out type handler for better separation

---
+++ src/Utilities.Common.Data/DefaultValueHandlerDictionary.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Utilities.Common.Data.Extensions;
+
+namespace Utilities.Common.Data
+{
+    internal sealed class DefaultValueHandlerDictionary
+    {
+        private readonly ConcurrentDictionary<Type, Func<object, object>> _typeHandlers;
+
+        public DefaultValueHandlerDictionary()
+        {
+            _typeHandlers = new()
+            {
+                [typeof(bool)] = HandleBoolean,
+                [typeof(string)] = HandleString
+            };
+        }
+
+        public ReadOnlyDictionary<Type, Func<object, object>> DefaultValueHandlers => new(_typeHandlers);
+
+        public void AddOrUpdate(IEnumerable<KeyValuePair<Type, Func<object, object>>> handlers)
+        {
+            foreach (KeyValuePair<Type, Func<object, object>> handler in handlers)
+            {
+                AddOrUpdate(handler.Key, handler.Value);
+            }
+        }
+
+        public void AddOrUpdate(Type type, Func<object, object> handler)
+        {
+            if (type is null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (handler is null)
+            {
+                throw new ArgumentNullException(nameof(handler));
+            }
+
+            _typeHandlers.AddOrUpdate(type, handler, (k, v) => handler);
+        }
+
+        public bool TryGetValue(Type type, out Func<object, object> handler)
+        {
+            if (_typeHandlers.TryGetValue(type, out Func<object, object> innerHandler))
+            {
+                handler = innerHandler;
+                return true;
+            }
+
+            handler = null;
+            return false;
+        }
+
+        private static object HandleBoolean(object value)
+        {
+            bool boolValue = false;
+            return StringExtensions.TryParseBoolean(value.ToString(), ref boolValue)
+                && boolValue;
+        }
+
+        private static object HandleString(object value)
+        {
+            return value.ToString().Trim();
+        }
+    }
+}
+
